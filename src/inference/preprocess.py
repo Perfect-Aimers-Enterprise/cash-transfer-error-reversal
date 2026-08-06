af141878f3bca437449fef69302466d49ae2554d
Perfect Aimers Enterprise <perfectaimersenterprise@gmail.com>
2026-08-06 17:41:52 +0100

    FaaA

--- src/inference/preprocess.py
+++ src/inference/preprocess.py
@@ -33,11 +33,39 @@ def preprocess_transaction(
     # Encode categorical fields
     # --------------------------
 
+    # for col in ["channel", "location"]:
+
+    #     if col in df.columns and col in encoders:
+
+    #         df[col] = encoders[col].transform(
+    #             df[col].astype(str)
+    #         )
+    # --------------------------
+    # Encode categorical fields
+    # --------------------------
+
+    # Default values if missing
+    if "channel" not in df.columns:
+        df["channel"] = "mobile"
+
+    if "location" not in df.columns:
+        df["location"] = "Abuja"
+
     for col in ["channel", "location"]:
 
-        df[col] = encoders[col].transform(
-            df[col].astype(str)
-        )
+        if col in encoders:
+
+            # Handle unseen categories
+            value = str(df[col].iloc[0])
+
+            if value not in encoders[col].classes_:
+                value = encoders[col].classes_[0]
+
+            df[col] = encoders[col].transform([value])
+        else:
+            # Encoder missing completely
+            df[col] = 0
+
 
     # --------------------------
     # Remove fields not used
